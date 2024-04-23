d99d13d8ef9b285056807671a1021e3a2e324870
Arthurdw <dev@arthurdw.com>
2024-04-23 16:09:13 +0200

    feat: completed the react general, jsx, vdom, reconciliation algorithm and the update process

---
+++ code/jsx_component.tsx
@@ -0,0 +1,4 @@
+function AnswerToTheUniverse() {
+    const theAnswer = 42;
+    return <p>The answer to the universe is: {theAnser}</p>;
+}
---
+++ code/jsx_interpolation.tsx
@@ -0,0 +1,2 @@
+const example = "NGWS";
+const element = <p>This variable is interpolated {example}</p>
